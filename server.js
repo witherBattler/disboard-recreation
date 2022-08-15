18a6d9a0223fbd137c3a48a0c3ef10e24b5ab2c9
IMPixel <asemenov.ca@gmail.com>
2022-08-15 15:50:44 -0400

    authentication

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const authRoute = require("./routes/auth")
+const session = require("express-session")
+const passport = require("passport")
+const discordStrategy = require("./strategies/discordStrategy")
 
-
-app.use(express.static('public'));
-app.use(bodyParser.json());
-app.use(cors());
 app.set("view engine", "ejs");
 app.listen(3000, () => console.log('http://localhost:3000'));
 
+// Middleware
+app.use("/auth", authRoute)
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(session({
+    secret: "ejs is the best",
+    cookie: {
+        maxAge: 60000 * 60 * 24
+    },
+    saveUninitialized: false,
+    resave: false
+}))
+
+// Passport
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get("/", (req, res) => {
     res.render("index");
